861fe8b603bd45a9ec7f1c87a2d70c9f5cbe10c2
adikmoi04 <johnmoises.nipas@gmail.com>
2019-12-07 14:32:59 +0800

    feature/contact: added contact.php

--- contact.php
+++ contact.php
@@ -10,7 +10,103 @@
   ?>
 
   <!-- Page Content -->
-  <div class="container"></div>
+  <div class="container">
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Small Business - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/small-business.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <!-- Navigation -->
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link " href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link " href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link active" href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <!-- /.col-lg-8 -->
+      <div class="col-lg-12">
+        <h1 class="font-weight-light">Page 3</h1>
+      </div>
+      <!-- /.col-md-4 -->
+    </div>
+    <!-- /.row -->
+
+    <div class = "row">
+      <div class = "col-lg-12 col-md-12 col-sm-12 mb-2">
+        <h3>Basic Info:</h3>
+      </div>
+
+      <div class = "col-lg-12">
+        <ul>
+          <li>
+            Address: 24 Geronimo Street, Sta Monica, Novaliches Quezon City, Philippines
+          </li>
+          <li>
+            Phone: 0966 355 3498
+          </li>
+          <li>
+            Address: <a href = "https://www.facebook.com/kalskills/">https://www.facebook.com/kalskills/</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class = "col-lg-12">
+        <h3>Location:</h3>
+      </div>
+
+      <div class = "col-lg-12">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.8382183568638!2d121.03989531484157!3d14.721736989725388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b14ed7c27a0d%3A0x629e025b45feec49!2sKal%20Skills%20Akademie%20Inc!5e0!3m2!1sen!2sph!4v1575592050164!5m2!1sen!2sph" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      </div>
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+ 
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+</div>
   <!-- /.container -->
 
   <?php include_once("./components/footer.php"); ?>
